3490676e23ed3819e96d5a6b7f0e06f40198ba77
nuhaBoston <39391126+nuhaBoston@users.noreply.github.com>
2019-09-13 11:55:21 +0200

    Update rockPaperScissors.py

---
+++ rockPaperScissors.py
@@ -0,0 +1,33 @@
+#when playing RPS the computer needs to randomly throw out a hand
+#therefore this should be pretty easy to make the computer play something
+#
+import random  
+
+def possiblePlays(a):
+    A = []
+    if a == 3:
+         A = ['rock', 'paper', 'scissors']
+    if a == 2:
+        A = ['rock', 'paper']
+    elif a == 1:
+        A = ['rock']
+    return A
+
+print(possiblePlays(3))
+
+#b can either be true or false
+def computerPlay(b):
+    num = random.randint(1,3)
+    if b:
+        if num == 1:
+            return 'rock'
+        if num == 2:
+            return 'paper'
+        if num == 3:
+            return 'scissors'
+
+
+    
+
+
+
