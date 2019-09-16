b2c9f9a87ffc820f16a00625bc94eae9d84f75a4
nuhaBoston <39391126+nuhaBoston@users.noreply.github.com>
2019-09-16 19:11:55 +0200

    Update rockPaperScissors.py

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -3,34 +3,57 @@
 #
 import random  
 
-def possiblePlays(a):
-    A = []
-    if a == 3:
-         A = ['rock', 'paper', 'scissors']
-    if a == 2:
-        A = ['rock', 'paper']
-    elif a == 1:
-        A = ['rock']
-    return A
-
-print(possiblePlays(3))
-
 #b can either be true or false
 def computerPlay(b):
     num = random.randint(1,3)
     if b:
-        if num == 1:
-            return 'rock'
-        if num == 2:
-            return 'paper'
-        if num == 3:
-            return 'scissors'
+        return num 
+
 
 def humanPlay(c):
-    return 
+    if c == 1:
+        return 1
+    if c == 2:
+        return 2
+    if c == 3:
+        return 3
+    else:
+        return 'You have played an invalid move. Try Again'
 
+## paper covers rock, scissors beats paper, rock beats scissors
+def whoWins(b,c):
+    if b == 1 and c == 2:
+        return 'You won!'
+    if b == 2 and c == 1:
+        return 'Computer Won!'
+    if b == 2 and c == 3:
+        return 'You won!'
+    if b == 3 and c == 2:
+        return 'Computer Won!'
+    if b == 1 and c == 3:
+        return 'Computer Won!'
+    if b == 3 and c == 1:
+        return 'You won!'
+    if (b == 1 and c == 1) or (b == 2 and c == 2) or (b==3 and c==3):
+        return "It's is a tie"
 
-    
+def Game(d):
+    print("Welcome to Rock, Paper, Scissors!\n")
+    print("You have the choice to play Rock, Paper, or Scissors\n")
+    print("Please Type in 1 for Rock, 2 for Paper, 3 for Paper\n")
+    playAgain = True
+    while(playAgain):
+        HumanMove = int(input("What's Your Move?\n"))
+        if HumanMove != 1 or HumanMove != 2 or HumanMove != 3:
+            print('You have played an invalid move.\n')
+            HumanMove = int(input("Try Again: "))
+        print(whoWins(humanPlay(HumanMove),computerPlay(HumanMove)))
+        yesOrNo = input("\nWould you like to play again? Please reply yes or no \n")
+        if yesOrNo == 'yes': 
+            playAgain = True
+        if yesOrNo == 'no':
+            playAgain = False
 
+    return None
 
-
+Game(1)
